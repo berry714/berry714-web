86432a81cb3827f787acbdd730dbfeb15074d0a5
berry714 <156739279+berry714@users.noreply.github.com>
2024-01-29 18:31:02 -0800

    Update test-sketch.js
    Update test-sketch.js to repeat animation upon the slowest squares reaching the end of the canvas.

--- test-sketch.js
+++ test-sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 300);
  
   rectMode(CENTER)
 }
@@ -8,26 +8,32 @@ function draw() {
    background(220);
   
   // Squares Moving Right
-  rect(originLeft + moveRight,75,50,50)
+  rect(originLeft - 25 + moveRight,75,50,50)
   
   rect(originLeft - 150 + 2 * moveRight, 175, 50, 50)
   
   rect(originLeft - 300 + 3 * moveRight, 275, 50, 50)
   
   // Squares Moving Left
-  rect(originRight + 300 + 3 * moveLeft, 125, 50, 50)
+  rect(originRight + 300 + 3 * moveLeft, 25, 50, 50)
   
-  rect(originRight + 150 + 2 * moveLeft, 225, 50, 50)
+  rect(originRight + 150 + 2 * moveLeft, 125, 50, 50)
   
-  rect(originRight + moveLeft, 325, 50, 50)
+  rect(originRight + 25 + moveLeft, 225, 50, 50)
   
-  
-  moveRight = moveRight + 1;
-  moveLeft = moveLeft - 1;
+  if (frameCount === 850 || frameCount === 1700 || frameCount === 2550 || frameCount === 3400) {
+    moveRight = 0;
+    moveLeft = 0;
+   
+  } else {
+          moveRight = moveRight + 1;
+          moveLeft = moveLeft - 1;
+  }
+    
   
 }
 
 var moveRight = 0;
 var moveLeft = 0;
 var originLeft = 0;
-var originRight = 400;
+var originRight = 800;
